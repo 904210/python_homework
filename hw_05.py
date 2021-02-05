e5070703f206be3e7207c1a444c96e0ea9dac0d4
Andrey <a_kharlamov@inbox.ru>
2021-02-05 20:17:50 +0300

    fixed

--- hw_05.py
+++ hw_05.py
@@ -3,14 +3,17 @@
 lst_rate = [7, 5, 3, 3, 2]
 print('Текущий рейтинг', lst_rate)
 
-while True:
-    print('для выхода введите 00')
-    n = int(input('Введите новый элемент рейтинга: '))
-    if n == 00:
-        break
-    elif n in lst_rate:
-        lst_rate.insert(lst_rate.index(n), n)
-        print(f'Пользователь ввел число {n}. Результат: ', *lst_rate)
-    else:
-        lst_rate.insert(0, n)
-        print(f'Пользователь ввел число {n}. Результат: ', *lst_rate)
+digit = int(input("Введите число (или 00 для выхода): "))
+while digit != 00:
+    for el in range(len(lst_rate)):
+        if lst_rate[el] == digit:
+            lst_rate.insert(el + 1, digit)
+            break
+        elif lst_rate[0] < digit:
+            lst_rate.insert(0, digit)
+        elif lst_rate[-1] > digit:
+            lst_rate.append(digit)
+        elif lst_rate[el] > digit and lst_rate[el + 1] < digit:
+            lst_rate.insert(el + 1, digit)
+    print(f"текущий список - {lst_rate}")
+    digit = int(input("Введите число "))
