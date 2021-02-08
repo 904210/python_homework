8795614fb858583917746ccae81053427aac6a09
Andrey <a_kharlamov@inbox.ru>
2021-02-08 20:21:02 +0300

    fix_tasks_04_and_06

--- les_03_hw_04.py
+++ les_03_hw_04.py
@@ -1,21 +1,32 @@
 def my_func_one(a, b):
-    """function exponentiation"""
+    """
+    function of exponentiation
+    you need to enter two variables:
+    number is a real positive integer
+    degree is a real negative integer
+    """
 
-    print(f'{a} ** {b} = ', a ** b)
+    print(f'{a} ** {b} = {round((a ** b), 2)}' if a > 0 and b < 0 else '-= invalid input =-')
 
 
 def my_func_two(x, y):
     """function exponentiation"""
 
     result = x
-    for _ in range(y - 1):
+    for _ in range(abs(y) - 1):
         result *= x
-    print(f'{x} ** {y} = ', result)
+    print(f'{x} ** {y} = {round(1 / result, 2)}' if x > 0 and y < 0 else '-= invalid input =-')
 
 
 help(my_func_one)
 numbers_one = [2, 3, 4]
-numbers_two = [2, 3, 4]
+numbers_two = [-2, -3, -4]
+
+# control values
+print('\ncontrol values:')
+for i in numbers_one:
+    for j in numbers_two:
+        print(f'{i} ** {j} = {round(pow(i, j), 2)}')
 
 # static test my_func_one
 print('\nstart of the static test my_func_one:')
@@ -35,4 +46,4 @@ my_func_one(int(input('input number: ')), int(input('input degree: ')))
 
 # manual test my_func_two:
 print('\nstart of the manual test my_func_two:')
-my_func_one(int(input('input number: ')), int(input('input degree: ')))
+my_func_two(int(input('input number: ')), int(input('input degree: ')))
--- les_03_hw_06.py
+++ les_03_hw_06.py
@@ -1,8 +1,8 @@
 def int_func(lst):
     """taking a string from small latin letters and returning it, but with a capital first letter"""
 
-    print(' '.join(lst).title())
+    return lst.title() if lst.islower() else '-= invalid input =-'
 
 
 help(int_func)
-int_func(input('input text: ').split())
+print(int_func(input('input text: ')))
